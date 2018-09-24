04f2a5b3a4d526443aa06df119a6409f6b57217d
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 11:59:24 -0300

    imprimindo variaveis

--- print.py
+++ print.py
@@ -1,5 +1,9 @@
+# Imprimindo um STRING
 print ("Imprimindo na tela.")
 
-var = 1
-
-print (var)
+# Imprimir um VARIAVEL
+# Criada variavel chamada nome_usuario
+# Variavel nome_usuario recebe o nome "marcelopoars"
+nome_usuario = "marcelopoars"
+# Mostra na tela o conteudo da variavel
+print (nome_usuario)
