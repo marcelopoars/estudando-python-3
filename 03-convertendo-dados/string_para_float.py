f3a6fbeebf68772df2b6914d9d09cfe680c30b64
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 20:59:22 -0300

    Criado arquivo string_para_float.py

---
+++ 03-convertendo-dados/string_para_float.py
@@ -0,0 +1,8 @@
+# Convertendo STRING para FLOAT
+numero_float_1 = float("12.5")
+numero_float_2 = float("12")
+numero_float_3 = float(71)
+
+print("Float 01:",numero_float_1,
+        "| Float 01:",numero_float_2,
+        "| Float 01:",numero_float_3)
---
+++ 03-convertendo-dados/string_para_inteiro.py
@@ -0,0 +1,8 @@
+# Convertendo STRING para INTEIRO
+numero = int("12")
+# Mostra na tela o conteudo da varial "numero"
+print("O número convertido é:", numero)
+
+# Outra maneira utilizando (input)
+valor_digitado = int(input("Digite um número inteiro: "))
+print("O número inteiro digitado é:", valor_digitado);
