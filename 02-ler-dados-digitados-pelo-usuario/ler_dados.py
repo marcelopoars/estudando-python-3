527443b66f95f5c6c1e5347f02a9f0a016d87dca
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 18:24:09 -0300

    criado arquivo ler_dados.py

---
+++ 02-ler-dados-digitados-pelo-usuario/ler_dados.py
@@ -0,0 +1,5 @@
+# Neste exemplo vamor receber dados do teclado digitados pelo usuario
+# Criamos uma variavel para receber o valor digitado pelo usuario
+valor = input("Por favor digite um numero: ")
+
+print("O valor digitado foi: ", valor)
