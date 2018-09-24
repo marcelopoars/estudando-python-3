7d130d1091869fb8782044720142107239b71d6a
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 19:19:29 -0300

    Recebendo dados via teclado - imprimindo na tela

--- 02-ler-dados-digitados-pelo-usuario/ler_dados.py
+++ 02-ler-dados-digitados-pelo-usuario/ler_dados.py
@@ -1,5 +1,12 @@
 # Neste exemplo vamor receber dados do teclado digitados pelo usuario
 # Criamos uma variavel para receber o valor digitado pelo usuario
 valor = input("Por favor digite um numero: ")
-
+# Aqui imprimimos na tela o numero digitado pelo usuario
 print("O valor digitado foi: ", valor)
+
+# Aqui vamos solicitar que o usuario digite seu NOME e sua IDADE
+# Logo apos vamos imprimir os dados digitados
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print("Nome: {} - Idade: {}" .format(nome, idade))
