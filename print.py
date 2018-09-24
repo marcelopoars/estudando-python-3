5fbffa3e7dcb307e47105254540b7162dff0c005
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 14:00:12 -0300

    Imprimindo dados de um dicionario.

--- print.py
+++ print.py
@@ -15,3 +15,13 @@ print ("Seja bem-vindo", nome_usuario,"!")
 nome_usuario = "Marcelo Pereira"
 senha = 21212121
 print("Olá {}! Sua senha é: {}" .format(nome_usuario, senha))
+
+# Imprimir utilizando um DICIONARIO
+# Criamos um dicionario chamado: dados_do_usuarios
+dados_do_usuarios = {
+"nome_usuario" : "João da Silva",
+"user_name" : "joaosilva",
+"senha" : 232323
+}
+# Imprime o conteudo do meu dicionario ("dados_do_usuarios")
+print("Olá {nome_usuario}! Seu User Name é: {user_name} e sua senha é {senha}" .format(**dados_do_usuarios))
