60ec2f4b7e60c53e383d74838534eea11e317032
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-09-24 21:04:05 -0300

    Criado arquivo string_para_float.py

--- 03-convertendo-dados/string_para_float.py
+++ 03-convertendo-dados/string_para_float.py
@@ -3,6 +3,4 @@ numero_float_1 = float("12.5")
 numero_float_2 = float("12")
 numero_float_3 = float(71)
 
-print("Float 01:",numero_float_1,
-        "| Float 01:",numero_float_2,
-        "| Float 01:",numero_float_3)
+print("Float 01: {}  | Float 01: {} | Float 01: {}".format(numero_float_1, numero_float_2, numero_float_3))
