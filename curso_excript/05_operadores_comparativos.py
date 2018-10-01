d2634d92fdc35d582ac9e228a720c91ec6917277
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-01 19:40:57 -0300

    Efetuando  comparações que retornam TRUE ou FALSE

--- curso_excript/05_operadores_comparativos.py
+++ curso_excript/05_operadores_comparativos.py
@@ -12,4 +12,9 @@ print("(>=) - maior ou igual a ...")
 print("(<=) - menor ou igual a ...")
 print("(==) - igual")
 print("(!=) - diferente")
-print(True)
+
+teste_1 = "a" == "a"
+teste_2 = "a" == "b"
+teste_3 = "a" != "b"
+teste_4 = 12 == "12"
+print("teste 1 = {} | teste 2 = {} | teste 3 = {} | teste 4 = {}" .format(teste_1, teste_2, teste_3, teste_4))
