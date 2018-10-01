4d6a0784cf61148fad764889350b2ac78cbaef10
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-01 19:28:36 -0300

    Estudando operadores comparativos

---
+++ curso_excript/05_operadores_comparativos.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Aula 19 - Operadores comparativos / relacionais
+- sao utilizados para verificar CONDIÇOES e julgarem se:
+VERDADEIRO (True) ou FALSO (False)
+"""
+print("=== Os exemplos abaixo retornam sempre TRUE ou FALSE =====")
+print("(>)   - maior que ...")
+print("(<)   - menor que ...")
+print("(>=) - maior ou igual a ...")
+print("(<=) - menor ou igual a ...")
+print("(==) - igual")
+print("(!=) - diferente")
+print(True)
