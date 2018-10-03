5184bb798d850695309590c97b3f0e14fe0f2338
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-02 22:05:03 -0300

    If, elif, else...

--- curso_excript/06_if_elif_else_2.py
+++ curso_excript/06_if_elif_else_2.py
@@ -15,8 +15,7 @@ print("\n ======== Perguntando ao usuário ========")
 acao = (input("Digite [1] para SIM ou [2] para NÃO: "))
 if acao == "1":
     print("Você disse SIM!")
+elif acao == "2":
+    print("Você disse NÃO!")
 else:
-    if acao == "2":
-        print("Você disse NÃO!")
-    else:
-        print("Você disse NÃO digitou nem [1] nem [2].")
+    print("Você disse NÃO digitou nem [1] nem [2].")
