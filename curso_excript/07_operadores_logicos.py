9e46d3bc66acbc1368f599d4b5d41d32da49fd22
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-02 23:45:00 -0300

    Estudando Operadores Lógicos

---
+++ curso_excript/07_operadores_logicos.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Aula 25 - Operadores Lógicos. """
+
+"""
+and - e
+or - ou
+not - negação
+is - é
+in not - não é
+in - está contido
+not in - não está contido
+"""
+
+print("2 > 4 = ", (2 > 4))
+print("2 > 2 = ", (2 > 4))
+print("2 > 1 = ", (2 > 4))
+print("4 > 2 = ", (2 > 4))
+print("2 >= 2 = ", (2 > 4))
+print("(2 > 2) or (3 < 5) = ", (2 > 2) or (3 < 5))
+print("type(2) is int", type(2), "=", type(2) is int)
+
+var = 2
+print((2 in var))
