e046d6eb6a4399af3b545316a03c3963108a7672
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-08 09:08:56 -0300

    Estudando Operadores Compostos

---
+++ 03_2_operador_atribuicao/03_operadores_compostos.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Operadores de Atribuição Compostos. """
+
+# Inicializando as variáveis X e Y
+x = 9
+y = 3
+# Agora vamos fazer alguns cálculos utilizando
+# Atribuidores Compostos
+
+x += y
+print(x)
+# Resetando variavel para valor original (9)
+x = 9
+
+x -= y
+print(x)
+# Resetando variavel para valor original (9)
+x = 9
+
+x *= y
+print(x)
+# Resetando variavel para valor original (9)
+x = 9
+
+x /= y
+print(x)
+# Resetando variavel para valor original (9)
+x = 9
+
+x %= y
+print(x)
+# Resetando variavel para valor original (9)
+x = 9
