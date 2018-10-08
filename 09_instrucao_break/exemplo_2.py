7e7d3af4f7e7617bede00c4d980c617f41187f39
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-08 20:43:01 -0300

    Estudando a instrução break

--- 09_instrucao_break/exemplo_2.py
+++ 09_instrucao_break/exemplo_2.py
@@ -3,6 +3,9 @@
 
 """ Exemplo 2 - Intrução break. """
 
+# A instrução "break" interrompe todos os cilcos
+# de um laço de repetição
+
 print("Antes de entrar no laço de repetição")
 for item in range(10):
     print(item)
