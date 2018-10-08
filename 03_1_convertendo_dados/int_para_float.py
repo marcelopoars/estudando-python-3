ce3d14ba50a8f613d7c7c4e824bdeafa115d506f
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-08 09:13:45 -0300

    Estudando conversão de dados

---
+++ 03_1_convertendo_dados/int_para_float.py
@@ -0,0 +1,6 @@
+# Aqui a variavel "numero_float" recebe um numero inteiro (71)
+# O numero inteiro é convertido em FLOAT
+numero_float = float(71)
+
+# Aqui é impresso na tela o valor da variavel convertido em float
+print("Inteiro convertido em float: {}" .format(numero_float))
