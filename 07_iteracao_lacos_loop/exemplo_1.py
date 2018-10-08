0f3eeb81ff05a1dcdbb69900af0ecd57d9a843ba
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-08 11:30:25 -0300

    Usando while e else

---
+++ 07_iteracao_lacos_loop/exemplo_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Iteração, Laços Condicionais e Loop. """
+
+#  #### Exemplo 1 ####
+
+# Variável (x) inicializada com valor (0)
+x = 0
+print("Executando a instrução while")
+#   Enquanto a condição for VERDADEIRA, executa o loop
+while x < 10:
+    print(x)
+    x += 1
+# Após executar o loop, executa o "else"
+else:
+    print("Executanto a instrução else")
+print("Fim")
