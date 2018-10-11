0c8741383c76d17a2097f557f48abb6618576c7e
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-10 21:42:20 -0300

    Estudando funções em Python

---
+++ 19_funcoes/exemplo_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Estudando funções no Python. """
+
+"""
+SINTAXE
+def nome_da_funcao():
+    bloco de instrução
+"""
+
+# esta função imprime uma linha na tela
+def mostrar_linhas():
+    print("-" * 50)
+
+
+def titulo(texto):
+    print("-" * 50)
+    print(texto)
+    print("-" * 50)
+
+
+# chamando a função "mostrar_linhas()"
+mostrar_linhas()
+
+# chamando a função "titulo()"
+titulo("TÍTULO 1 - Estudando funções")
+titulo("TÍTULO 2 - Aprendendo a programar em Python")
