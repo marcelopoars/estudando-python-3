6da7a6958b704b3f5b245d0e5b383669e4d7f4b2
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-15 21:45:08 -0300

    Estudando TUPLAS

--- 12_tuplas/exemplo_1.py
+++ 12_tuplas/exemplo_1.py
@@ -3,17 +3,29 @@
 
 """ Exemplo1 - Estudando TUPLAS. """
 
-tupla = (11, 22, 33, "tt")
+# elementos delimitados por um "par de parenteses"
+# são para o Python uma "Tupla"
+tupla1 = (11, 22, 33, "tt")
+
+# elementos separados por "vírgula"
+# são para o Python uma "Tupla"
+tupla2 = "aaa", 1, True
 
 # A função "tuple()" converte a string para tupla
-variavel = tuple("Python")
+tupla3 = tuple("Python")
 
-# imprimindo o conteúdo da tupla
-print(variavel)
-# imprimindo a variavel "tupla"
-print(tupla)
+# inicializando uma tupla vazia
+tupla4 = ()
+tupla4 = tupla1
+
+# imprimindo o conteúdo das tuplas
+print(tupla1)
+print(tupla2)
+print(tupla3)
+print(tupla4)
 
 # imprimindo o tipo da variavel
-print(type(tupla))
-print(type(variavel))
+print(type(tupla1))
+print(type(tupla2))
+print(type(tupla3))
 print(type( (1, 2, 3) ))
