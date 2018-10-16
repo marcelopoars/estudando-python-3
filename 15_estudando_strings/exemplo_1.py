2bd1f101db9533a1e49aedca94ca382d111f0b1b
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-16 00:15:40 -0300

    estudando strings

---
+++ 15_estudando_strings/exemplo_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Estudando Strings. """
+
+# 04 formas para declarar Strings
+s1 = 's1 = String com aspas simples'
+s2 = "s2 = String com aspas dupla"
+s3 = '''s3 = String com 3 aspas simples
+        String com 3 aspas simples
+        String com 3 aspas simples'''
+s4 = """s4 = String com 3 aspas
+        dupla String com 3 aspas dupla
+        String com 3 aspas dupla"""
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
---
+++ 15_estudando_strings/exemplo_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Estudando Strings. """
+
+s = "Para o Python, toda String é uma lista imutável."
+
+# imprime o primeiro caracter da String
+print(s[0])
+
+# para o Python cada caracter de uma String é uma "String"
+print(type(s[0]))
+
+# imprime o último caracter da string
+print(s[-1])
+
+# fatiando a String
+print(s[2:20])
+
+# invertendo a String
+print(s[:: -1])
