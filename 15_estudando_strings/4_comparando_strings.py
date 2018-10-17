1ea6b334c3b77640fa7cc2d30c082cd933e5c3bb
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-17 17:05:59 -0300

    Manipulando arquivos com Python.

--- 15_estudando_strings/4_comparando_strings.py
+++ 15_estudando_strings/4_comparando_strings.py
@@ -13,4 +13,19 @@ quando estamos trabalhando com o sistema ASCII.
     e nos é retornado a respectiva letra.
 """
 
-print()
+# comparando caracteres
+# deve retornar (True ou False)
+# o Python toma por base o "código na TABELA ACII"
+print("a" > "X")
+print("a" > "A")
+print("a" > "10")
+
+# a função "chr()" retorna o "código" da tabela ASCII
+print(chr(100))
+print(chr(79))
+
+# a função "ord()" retorna o "caracter" da tabela ASCII
+print(ord("d"))
+
+for cod in range(128):
+    print(f"- {cod} = {chr(cod)} | Bin: {bin(cod)} | Exa: {hex(cod)}")
