4b6afa2833855e6e5586b9c62c689f0b0aa76b99
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-18 17:19:50 -0300

    Estudando dicionário de dados.

--- 18_dicionario_de_dados/exemplo_1.py
+++ 18_dicionario_de_dados/exemplo_1.py
@@ -6,9 +6,45 @@
 # Imprimir utilizando um DICIONARIO
 # Criamos um dicionario chamado: dados_do_usuarios
 usuarios = {
+    # chave | valor
     "nome": "João da Silva",
     "user": "joaosilva",
     "senha": 232323
 }
+
+# imprime conteúdo do dicionário
+print(usuarios)
+
+# retorna o valor referente a chave "nome"
+print(usuarios["nome"])
+
 # Imprime o conteudo do meu dicionario ("dados_do_usuarios")
+# (**) expande o dicionário
 print("Olá {nome}! Seu User é: {user} | Senha é: {senha}".format(**usuarios))
+
+# a função "len()" retorna o tamanho do dicionário
+print(f"O dicionário de dados tem {len(usuarios)} elementos.")
+
+# a função "del()" exclui um elemento do dicionário
+del(usuarios["nome"])
+
+# agora o dicionário tem apenas "2" elementos
+print(usuarios)
+
+# a função "keys()" retorna apenas as chaves do dicionário
+print(usuarios.keys())
+print(type(usuarios.keys()))
+
+# a função "values()" retorna os valores do dicionário
+print(usuarios.values())
+
+# a função "grt()" retorna o valor vinculado a chave informada
+print(usuarios.get("user"))
+
+# a função "popitem()" retorna um elemento
+# em seguida remove mesmo do dicionário
+print(usuarios.popitem())
+# chamando a função "popitem()" mais uma vez
+print(usuarios.popitem())
+# agora o dicionário está vazio
+print(usuarios)
