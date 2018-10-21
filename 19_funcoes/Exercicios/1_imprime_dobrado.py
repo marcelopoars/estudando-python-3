a46c99e46e436c8a23fc22ae4103449964b96c90
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-21 13:42:07 -0200

    Estudnado funções.

---
+++ 19_funcoes/Exercicios/1_imprime_dobrado.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Como exercício, escreva um chamada a imprimeDobrado
+que imprima 'Spam'*4 'Spam'*4.
+Dica: strings podem ser colocadas tanto entre aspas simples
+quanto duplas e o tipo de aspas que não for usado para envolver
+a string pode ser usado dentro da string, como parte dela.
+"""
+
+
+def imprime_dobrado():
+    print("'Spam'*4 " * 2)
+
+
+imprime_dobrado()
---
+++ 19_funcoes/Exercicios/2_calcula_distancia.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Criar uma função chamada "distancia()"
+que calcule a distância entre dois pontos,
+dados pelas coordenadas (x1,y1) e (x2,y2).
+Dica: TOmar base pelo teorema de Pitágoras:
+Ex: distancia = V (x2 - x1)2 + (y2 - y1)2
+"""
+
+import math
+
+
+def distancia(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    dquadrado = dx ** 2 + dy ** 2
+    resultado = math.sqrt(dquadrado)
+    print("resultado = ", resultado)
+    return 0.0
+
+
+distancia(1, 2, 4, 6)
+
+a = 2
+b = 2
+
+print(id(a))
+print(id(b))
