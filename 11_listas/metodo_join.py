d6a29d420f7c041742d83567410f7f22b155a708
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-30 19:11:35 -0300

    Estudando método join() em listas.

---
+++ 11_listas/metodo_join.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"Estudando o Método join() em listas."
+
+# Exemplo 1
+lista_de_compras = ["pão", "presunto", "queijo", "café", "leite"]
+print("\n".join(lista_de_compras))
+
+# Exemplo 2
+print(" - ".join(lista_de_compras))
