2ca48aaaa4db8dce68aa6ad6c8ae8c7bae0848e2
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-10-30 19:44:18 -0300

    Estudando TUPLAS.

---
+++ 12_tuplas/exemplo_00.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"Estrutura de dado - TUPLA."
+
+# declarando a variável do tipo TUPLA
+altura, largura, profundidade = 10, 22, 45
+print("As dimensões do objeto são: {}cm x {}cn x {}cm".format(
+    altura, largura, profundidade))
