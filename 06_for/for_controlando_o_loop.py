c150a11711e3f5e6e35eeb187095545d214ec328
marcelopoars <marcelo.fatecpoa@gmail.com>
2018-11-02 10:08:26 -0300

    Estudando controle de loop com FOR.

--- 06_for/for_controlando_o_loop.py
+++ 06_for/for_controlando_o_loop.py
@@ -8,3 +8,10 @@ for number in numbers:
     if number > 6:
         print(number)
 print("We printed 7.")
+
+
+print("=== Exemplo 2 ===")
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+for item in a:
+    if item % 2 == 0:
+        print(item)
