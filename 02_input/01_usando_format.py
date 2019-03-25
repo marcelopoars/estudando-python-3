1eae337cf33b6340f73a9942a622ea6bb948db97
marcelopoars <marcelo.fatecpoa@gmail.com>
2019-03-25 17:58:16 -0300

    Organizando repositório.

---
+++ 02_input/01_usando_format.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Recebendo dados do usuário com (input). """
+
+# Vamos criar duas variáveis
+nome = input("Login: ")
+sobrenome = input("Senha: ")
+
+print("Olá {} {}! Seja bem vindo." .format(nome, sobrenome))
+
