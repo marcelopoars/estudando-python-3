c3d913920aa58e238f0042f1190485a1120fb9e7
marcelopoars <marcelo.fatecpoa@gmail.com>
2019-03-25 15:24:05 -0300

    Uso da função print.

--- 01_print/exemplo_1.py
+++ 01_print/exemplo_1.py
@@ -6,7 +6,12 @@
 # Aqui chamamos a função print e dentro dos parenteses e das aspas
 # colocamos o nosso texto que será impresso na tela
 print("Bora estudar Python pessoal!")
-print(2 + 2)
-print("3" + "3")
-print("Você entendeu porque (4) e (33) foram impressos?")
+
+# Aqui vamos imprimir a soma de dois inteiros
+# Note que escrevemos os número sem as (" ")
+print(2 + 5)
+
+# Aqui estamos concatenando duas string, visto que utilizamos as (" ")
+print("2" + "5")
+
 
