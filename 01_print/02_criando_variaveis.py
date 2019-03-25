c206dbe34ba868041a3a872b8bb807922b5cf06d
marcelopoars <marcelo.fatecpoa@gmail.com>
2019-03-25 17:48:29 -0300

    Organizando repositório.

---
+++ 01_print/02_criando_variaveis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" Criando variável e atribuindo valores."""
+
+# Aquivamos driar duas variáveis e atribuir um conteúdo
+nome = "Marcelo"
+sobrenome = "Pereira"
+
+#  Aqui criamos outra variável para receber "nome + sobrenome"
+# Note que vamos concatenar também um espaço (" ") para separar nome e sobrenome
+nome_completo = nome + " " + sobrenome
+
+#  Aqui vamos imprimir o resultado
+print(nome_completo)
